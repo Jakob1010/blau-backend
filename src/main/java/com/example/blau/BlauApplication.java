8476e8d54db2b9bd94be2227c0bd6eb1beafb9a2
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2023-08-11 20:31:41 +0200

    Initialize Spring application.

---
+++ src/main/java/com/example/blau/BlauApplication.java
@@ -0,0 +1,13 @@
+package com.example.blau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlauApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlauApplication.class, args);
+	}
+
+}
