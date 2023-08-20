6e4b656d7c1b3e2dbf2468eccd59691b2bc5b5aa
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2023-08-20 16:59:21 +0200

    Added DrinkEndpoint.

--- src/main/java/com/blau/mapper/DrinkMapper.java
+++ src/main/java/com/blau/mapper/DrinkMapper.java
@@ -1,8 +1,12 @@
 package com.blau.mapper;
 
 import com.blau.dto.DrinkDto;
+import com.blau.dto.LogDto;
 import com.blau.entity.Drink;
+import com.blau.entity.Log;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.mapstruct.Mapper;
+import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring")
 public interface DrinkMapper {
@@ -10,5 +14,7 @@ public interface DrinkMapper {
 
     DrinkDto map(Drink drink);
 
-
+    default Page<DrinkDto> toDtoPage(@NotNull Page<Drink> page) {
+        return page.map(this::map);
+    }
 }
--- src/main/java/com/blau/rest/DrinkEndpoint.java
+++ src/main/java/com/blau/rest/DrinkEndpoint.java
@@ -1,14 +1,37 @@
 package com.blau.rest;
 
+import com.blau.dto.DrinkDto;
+import com.blau.dto.LogDto;
+import com.blau.mapper.DrinkMapper;
+import com.blau.mapper.LogMapper;
+import com.blau.service.DrinkService;
+import com.blau.service.impl.LogServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(LogEndpoint.BASE_URL)
+@RequestMapping(DrinkEndpoint.BASE_URL)
 @Slf4j
 public class DrinkEndpoint {
     public static final String BASE_URL =  "/api/v1/drinks";
+    private final DrinkService drinkService;
+    private final DrinkMapper drinkMapper;
 
-    
+    public DrinkEndpoint(DrinkService drinkService, DrinkMapper drinkMapper) {
+        this.drinkService = drinkService;
+        this.drinkMapper = drinkMapper;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public Page<DrinkDto> getAllDrinks(Pageable pageable) {
+        log.info("GET {}", BASE_URL);
+        return drinkMapper.toDtoPage(drinkService.finAllDrinks(pageable));
+    }
 }
