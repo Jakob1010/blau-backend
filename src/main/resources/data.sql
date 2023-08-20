ae751eb60b62ea2ce8fdd2192684cbd69747a1af
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2023-08-20 12:55:20 +0200

    Update data.sql so that it ignores duplicates on insert.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-INSERT INTO drink(id,alcoholic,description,name) VALUES(1,'1','','Beer');
+INSERT INTO drink(id,alcoholic,description,name) VALUES(1,'1','','Beer')
+ON CONFLICT (id) DO NOTHING;
+
 INSERT INTO app_user(id,username,password,role) VALUES(1,'jakob','password','ROLE_ADMIN')
+ON CONFLICT (id) DO NOTHING;
