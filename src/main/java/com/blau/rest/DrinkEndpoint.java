a7eea9f9b2f99e71387febeb3bd6f9b1b6877d24
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2023-09-03 20:34:24 +0200

    Added getDrink and createDrink to DrinkEndpoint.

--- src/main/java/com/blau/rest/DrinkEndpoint.java
+++ src/main/java/com/blau/rest/DrinkEndpoint.java
@@ -10,7 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,4 +38,20 @@ public class DrinkEndpoint {
         log.info("GET {}", BASE_URL);
         return drinkMapper.toDtoPage(drinkService.finAllDrinks(pageable));
     }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public DrinkDto getDrink(@PathVariable Long id){
+        log.info("GET {}/{}", BASE_URL, id);
+        return drinkMapper.map(drinkService.getDrinkById(id));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> createDrink(@RequestBody DrinkDto drinkDto) {
+        log.info("POST {}", BASE_URL);
+        drinkService.save(drinkMapper.map(drinkDto));
+        return ResponseEntity.status(201).body("Drink created successfully.");
+    }
+
 }
