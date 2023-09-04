3c69a24bf1702535da3ae5e5478496c16c01ffeb
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2023-09-04 23:18:49 +0200

    Added CorsConfig and updated test data.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
 INSERT INTO drink(id,alcoholic,description,name) VALUES(1,'1','','Beer')
 ON CONFLICT (id) DO NOTHING;
 
+INSERT INTO drink(id,alcoholic,description,name) VALUES(2,'1','','Wine')
+    ON CONFLICT (id) DO NOTHING;
+
 INSERT INTO app_user(id,username,password,role) VALUES(1,'jakob','password','ROLE_ADMIN')
 ON CONFLICT (id) DO NOTHING;
