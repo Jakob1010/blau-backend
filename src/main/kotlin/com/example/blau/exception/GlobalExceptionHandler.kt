6e1010fddec7e4fcb8f1e50b42be49d445ba6f2c
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2025-03-22 14:20:33 +0100

    update exception handler

--- src/main/kotlin/com/example/blau/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/blau/exception/GlobalExceptionHandler.kt
@@ -1,16 +1,19 @@
 package com.example.blau.exception
 
 import org.slf4j.LoggerFactory
-import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties.Application
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalExceptionHandler {
-    private val log = LoggerFactory.getLogger(Application::class.java)
+    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
 
     @ExceptionHandler(Exception::class)
-    fun handleAllOtherExceptions(ex: Exception) {
-        log.error("An unexpected error occurred ${ex.message}")
+    fun handleAllOtherExceptions(ex: Exception): ResponseEntity<String> {
+        log.error("An unexpected error occurred: ${ex.message}", ex)
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body("An unexpected error occurred. Please try again later.")
     }
 }
