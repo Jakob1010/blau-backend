7fffbef7985748f58d8acf84f0a20663fdcd48bc
Jakob Aichinger <jakob.aichinger@tuwien.ac.at>
2025-04-11 21:49:39 +0200

    start implementing map feat

--- src/main/kotlin/com/example/blau/controller/ActivityLogController.kt
+++ src/main/kotlin/com/example/blau/controller/ActivityLogController.kt
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 import java.util.UUID
 
 @RestController
@@ -28,6 +31,19 @@ class ActivityLogController(private val service: ActivityLogService) {
         @RequestParam(required = false) activityIds: List<UUID>?
     ): ResponseEntity<List<ActivityLogDto>> {
         val activityLogs = service.getActivityLogsByUserId(userId, activityIds)
+        return ResponseEntity.ok(activityLogs)
+    }
+
+    @GetMapping("/withFriends/{userId}")
+    fun getActivityLogsWithFriendsByUserId(
+        @PathVariable userId: UUID,
+        @RequestParam(required = false) from: LocalDateTime?,
+        @RequestParam(required = false) to: LocalDateTime?
+    ): ResponseEntity<List<ActivityLogDto>> {
+        val today = LocalDate.now()
+        val startOfDay = from ?: today.atStartOfDay()
+        val endOfDay = to ?: today.atTime(LocalTime.MAX)
+        val activityLogs = service.getActivityLogsWithFriendsByUserId(userId, startOfDay, endOfDay)
         return ResponseEntity.ok(activityLogs)
     }
 }
